9ecdca7c3f65da64a54940e445ffc9501274e2f0
hanum-lee <chococloudy@gmail.com>
2020-04-14 00:27:19 -0600

    removed unnessecery file

--- FInal Project/Heating_test/Heating_test.ino
+++
@@ -1,8 +0,0 @@
-int led = 7; // the pin the LED is connected to
-void setup() {
-  pinMode(led, OUTPUT); // Declare the LED as an output
-}
-
-void loop() {
-  digitalWrite(led, HIGH); // Turn the LED on
-}
